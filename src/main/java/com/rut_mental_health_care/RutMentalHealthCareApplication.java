6f9a46264bb03f5014bf628ebbe7bb7f5db522c2
VAXA99 <vaxa99@icloud.com>
2023-09-10 00:18:46 +0300

    init

---
+++ src/main/java/com/rut_mental_health_care/RutMentalHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.rut_mental_health_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutMentalHealthCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutMentalHealthCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rut_mental_health_care/RutMentalHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rut_mental_health_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutMentalHealthCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
