56d137e15af15d47810823c7eb38b0484474212f
Acustic_Evil <prisha931@gmail.com>
2023-11-15 23:48:46 +0300

    deleted 1 unused import

--- src/main/java/com/rut_mental_health_care/service/consultation/ConsultationServiceImpl.java
+++ src/main/java/com/rut_mental_health_care/service/consultation/ConsultationServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
