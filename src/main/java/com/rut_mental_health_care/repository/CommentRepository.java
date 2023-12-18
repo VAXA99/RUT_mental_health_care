a12adeef1f39e5a6c66eb73c59063e075a8c0da9
VAXA99 <vaxa993@icloud.com>
2023-12-18 18:23:55 +0300

    added comment hierarchy handling in CommunicationServiceImpl

--- src/main/java/com/rut_mental_health_care/repository/CommentRepository.java
+++ src/main/java/com/rut_mental_health_care/repository/CommentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findCommentByPostId(Long postId);
+    List<Comment> findCommentByPostIdAndParentCommentIsNull(Long postId);
 }
