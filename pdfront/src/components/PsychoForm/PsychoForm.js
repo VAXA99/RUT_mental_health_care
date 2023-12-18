c9ce74a04c904cb7388e070bba6164f27709aa5e
IvaLLka <kazakivan555@yandex.ru>
2023-12-18 12:16:33 +0300

    add calendarPage for psychologist

--- pdfront/src/components/PsychoForm/PsychoForm.js
+++ pdfront/src/components/PsychoForm/PsychoForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Menu from '../Menu/Menu';
 import "./psychoForm.css"
+import Calendar from "../Calendar/Calendar";
+import CalendarComponent from "../Calendar/CalendarComponent";
 
 
 export default function PsychoForm() {
@@ -23,7 +25,7 @@ export default function PsychoForm() {
                 <div className="container main psycho__form">
                     <table className="psycho__form__head">
                         <thead className="week__days">
-                        <th>Пн</th>
+                        <th></th>
                             <th>Пн</th>
                             <th>Вт</th>
                             <th>Ср</th>
@@ -31,21 +33,66 @@ export default function PsychoForm() {
                             <th>Пт</th>
                             <th>Сб</th>
                             <th>Вс</th>
-                        <th>Вс</th>
+                            <th></th>
                         </thead>
                         <tbody className="week__numbers">
-                        <button type={"button"} className='month__button'> {'<'} </button>
-                            <td className='week__numbers__element'>30</td>
+                            <button type={"button"} className='month__button'>
+                                <img src="/img/кнопка%20влево.png" alt="" width='80%'/>
+                            </button>
+                            <td className='week__numbers__element focus'>30</td>
                             <td className='week__numbers__element'>31</td>
                             <td className='week__numbers__element'>1</td>
                             <td className='week__numbers__element'>2</td>
                             <td className='week__numbers__element'>3</td>
                             <td className='week__numbers__element'>4</td>
                             <td className='week__numbers__element'>5</td>
-                        <button type={"button"}  className='month__button'> {'>'} </button>
+                            <button type={"button"}  className='month__button'>
+                                <img src="/img/кнопка%20вправо.png" alt=""  width='80%'/>
+                            </button>
                         </tbody>
                     </table>
-
+                    <div className="display__flex__mt">
+                        <div className='sub__form__cont psycho__form'>
+                            <div>
+                                <div className='recording__row'>
+                                    <div className='left__spans'>
+                                        <div className='recording__number'>1</div>
+                                        <div className='recording__time'>8:00-9:30</div>
+                                    </div>
+                                    <div className='recording__name'>Смирнов Алексей</div>
+                                </div>
+                                <div className='recording__row'>
+                                    <div className='left__spans'>
+                                        <div className='recording__number'>2</div>
+                                        <div className='recording__time'>9:40-11:10</div>
+                                    </div>
+                                    <div className='recording__name'>Смирнов Алексей</div>
+                                </div>
+                                <div className='recording__row'>
+                                    <div className='left__spans'>
+                                        <div className='recording__number'>3</div>
+                                        <div className='recording__time'>11:20-12:50</div>
+                                    </div>
+                                    <div className='recording__name'>Смирнов Алексей</div>
+                                </div>
+                                <div className='recording__row focus'>
+                                    <div className='left__spans focus'>
+                                        <div className='recording__number'>4</div>
+                                        <div className='recording__time'>13:20-14:50</div>
+                                    </div>
+                                    <div className='recording__name'>Окно</div>
+                                </div>
+                                <div className='recording__row'>
+                                    <div className='left__spans'>
+                                        <div className='recording__number'>5</div>
+                                        <div className='recording__time'>15:00-16:30</div>
+                                    </div>
+                                    <div className='recording__name'>Смирнов Алексей</div>
+                                </div>
+                            </div>
+                        </div>
+                        <CalendarComponent/>
+                    </div>
                 </div>
 
             </div>
